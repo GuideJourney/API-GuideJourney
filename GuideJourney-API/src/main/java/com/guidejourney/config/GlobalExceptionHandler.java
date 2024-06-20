42e9ec663c4876491ae4472850b165ce39837a99
Jefferson03SA <jsantillana3@upao.edu.pe>
2024-06-19 20:10:47 -0500

    Agregar la informacion del estudiante

--- GuideJourney-API/src/main/java/com/guidejourney/config/GlobalExceptionHandler.java
+++ GuideJourney-API/src/main/java/com/guidejourney/config/GlobalExceptionHandler.java
@@ -8,18 +8,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.guidejourney.exceptions.InvalidProfileTypeException;
 import com.guidejourney.exceptions.UserNotFoundException;
 
-
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidProfileTypeException.class)
     public ResponseEntity<String> handleInvalidProfileTypeException(InvalidProfileTypeException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
